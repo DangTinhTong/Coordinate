494a36cbd4130953428f64249b308f3465ad58e9
admin <dangtinhtong@gmail.com>
2017-10-14 14:24:12 +0700

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+This is a program coordinates.
+Have to try hard.
+Don't be lazy!! (^__^).
--- coordinate/main.m
+++ coordinate/main.m
@@ -5,13 +5,48 @@
 //  Created by admin on 10/14/17.
 //  Copyright © 2017 admin. All rights reserved.
 //
-
+/*Define a class called XYPoint that will hold a Cartesian coordinate (x, y), where x and y are integers. Define methods to individually set the x and y coordinates of a point and retrieve their values.Write an Objective-C program to implement your new class and test it.*/
 #import <Foundation/Foundation.h>
 
+//--------interface section
+@interface XYpoint : NSObject
+{
+    int coordinateX;
+    int coordinateY;
+}
+
+-(void) setCoordinateX:(int) x;
+-(void) setCoordinateY:(int) y;
+-(int)  coordinateX;
+-(int)  coordinateY;
+@end
+//----------implementation section
+@implementation XYpoint
+
+-(void) setCoordinateX:(int)x{
+    coordinateX =x;
+    
+}
+-(void) setCoordinateY:(int)y {
+    coordinateY =y;
+}
+
+-(int)  coordinateX{
+    return coordinateX;
+}
+-(int) coordinateY{
+    return coordinateY;
+}
+@end
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+  
+    XYpoint *myXYpoint = [[XYpoint alloc] init];
+    [myXYpoint setCoordinateX: 1];
+    [myXYpoint setCoordinateY:2];
+    NSLog(@"The coordinate  (%i,%i)",
+          [myXYpoint coordinateX], [myXYpoint coordinateY]);
+    
+    
     return 0;
 }
